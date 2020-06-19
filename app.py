3f502e8fd71771160cecc83392179d413502daef
leminh0796 <leminh0796@gmail.com>
2020-06-19 16:24:22 +0700

    Thêm uploadfile()

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
 from flask import Flask, flash, request, redirect, url_for
 import os
 from werkzeug.utils import secure_filename
+from flask_cors import CORS, cross_origin
+import pandas as pd
 
 UPLOAD_FOLDER = "/tmp"
 ALLOWED_EXTENSIONS = {'csv'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+cors = CORS(app, resources = {r"/api/*": {"origins": "*"}})
 
 
-@app.route("/show_schema", methods=['GET'])
+@app.route("/showschema", methods=['GET'])
 def show_schema():
     pass
 
 
-@app.route("/process/<file_id>")
-def algorithm():
-    df = pd.read_csv("/tmp" + filename)
+@app.route('/api/uploadfile', methods=['POST', 'GET'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['input']
+        print(file.name)
+        file.save(file.name + ".csv")
+        return redirect(request.url)
+    return 'THIS IS BACKEND! THERE IS NOTHING HERE!'
 
+@app.route('/showinput', methods=['GET'])
+def show_input():
+    df = pd.read_csv('input.csv')
+    return str(df.shape[0])
